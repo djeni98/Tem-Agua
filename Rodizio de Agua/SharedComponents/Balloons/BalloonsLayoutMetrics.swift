051a06805d8b2a877a23fd1c91b09eb39816b016
Djenifer R. P <djeniferrenata@yahoo.com.br>
2021-10-06 19:09:18 -0300

    Create balloons views

---
+++ Rodizio de Agua/SharedComponents/Balloons/BalloonsLayoutMetrics.swift
@@ -0,0 +1,15 @@
+//
+//  BalloonsLayoutMetrics.swift
+//  Rodizio de Agua
+//
+//  Created by Djenifer Renata Pereira on 06/10/21.
+//
+
+import UIKit
+
+struct BalloonsLayoutMetrics {
+    static let cornerRadius: CGFloat = 10
+    static let shadowRadius: CGFloat = 4
+    static let shadowOpacity: Float = 0.25
+    static let shadowOffset: CGFloat = 4
+}
